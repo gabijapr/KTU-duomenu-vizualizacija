1f08af4455e8315266d6f29eeeff9f2f67858187
Gabija Pranaitytė <66127245+gabijapr@users.noreply.github.com>
2021-03-09 22:53:22 +0200

    Added code for the assignment

---
+++ laboratorinis/R/lab.R
@@ -0,0 +1,58 @@
+setwd("C:/Users/gabij/Desktop/Mokslai/4SEMESTRAS/Programavimas_viz/KTU-duomenu-vizualizacija/laboratorinis")
+
+library(tidyverse)
+
+# duomenu skaitymas
+data0<-read_csv("data/lab_sodra.csv")
+
+# atfiltravimas pagal ekonomines veiklos koda
+data<-data0 %>% filter(ecoActCode==479100)
+
+# 1
+summary(data$avgWage)
+# pirmojo grafiko kodas
+ggplot(data, aes(x=avgWage))+
+        geom_histogram(fill="darkslategrey", alpha=0.8, col="black")+
+        labs(title = "Average wage of employees (economic activity code: 479100)",
+             x="Average Wage, euros", y="Count") + theme_bw()
+
+
+dev.copy(png, file="img/plot1.png", width=780)
+dev.off()
+
+# 2
+# menesio isskyrimas is datos kintamojo
+data <- data %>% mutate(month_value=as.integer(substr(month, 5 ,7)))
+# top 5 imoniu atrinkimas pagal avgWage
+topcompanies<-data %>% group_by(name) %>% slice_max(avgWage, n=1) %>% ungroup() %>%
+        top_n(avgWage, n=5) %>% select(name)
+# top 5 imoniu duomenu atrinkimas 
+top5 <- data %>% filter(name %in% topcompanies$name)
+# antrojo grafiko kodas
+ggplot(top5, aes(x=month_value, y=avgWage, col=name))+
+        geom_line()+
+        geom_point()+
+        scale_x_continuous("month",breaks=1:12,limits=c(1,12))+
+        theme_bw()+
+        labs(title= "Average wage of employees by month",
+             x="Month", y="Average Wage")
+
+dev.copy(png, file="img/plot2.png", width=780)
+dev.off()
+
+# 3
+# treciojo grafiko kodas
+top5 %>%  group_by(name) %>% top_n(numInsured,n=1) %>% distinct(name,numInsured) %>%
+        ggplot(aes(x = (reorder(name,-numInsured)), y = numInsured, fill=name))+
+        geom_col()+
+        labs(title= "# of employees insured",
+             x="Company", y="# of emplyees") +
+        theme_bw()+
+        scale_x_discrete(guide = guide_axis(n.dodge = 2))
+
+dev.copy(png, file="img/plot3.png", width=780)
+dev.off()
+
+
+
+
--- laboratorinis/README.md
+++ laboratorinis/README.md
@@ -8,26 +8,28 @@
 
 Atsakymas:
 
-![histograma](img/pavyzdys1.png)
+![histograma](img/plot1.png)
 
-Išvados:
+Išvados: Ekonominės veiklos kodo 479100 įmonėse vidutinis atlyginimas yra pasiskirstęs normaliuoju skirstiniu N~(961,518). Vidutinis atlyginimas yra 961 eur, standartinis nuokrypis - 518 eur, mediana - 886 eur.
 
 ### 2. Užduotis
 
 Atsakymas:
 
-![atlyginimai](img/pavyzdys2.png)
+![atlyginimai](img/plot2.png)
 
-Išvados:
+Išvados: Top 5 įmonės, turinčios didžiausią atlyginimą yra "UŽDAROJI AKCINĖ BENDROVĖ SELECT RINKA", "UŽDAROJI AKCINĖ BENDROVĖ ARPRESA", "UAB HARMONY LIFE", "UAB CGTRADER", "UAB VOLTAS IT".  Įmonių "UAB HARMONY LIFE" ir "UAB CGTRADER" atlyginimų kaita 2020 metų eigoje išliko stabili.
+UAB "SELECT RINKA" duomenys stebimi iki rugpjūčio mėnesio, per kurį darbuotojų atlyginimas išaugo maždaug trigubai. Tolesnėje metų eigoje atlyginimo stebinių sodros duomenyse nėra.
+Likusių dviejų įmonių, "APRESA" ir "VOLTAS IT", atlyginimai 2020 m. laikotarpiu ženkliai nesikeitė, išskyrus gruodžio mėnesį. Toks pakilimas, galima spėti, atsirado dėl to, kad kai kuriose įmonėse yra praktika paskutinį mėnesį mokėti priedus.
 
 
 ### 3. Užduotis
 
 Atsakymas:
 
-![apdraustieji](img/pavyzdys3.png)
+![apdraustieji](img/plot3.png)
 
-Išvados:
+Išvados: Lyginant apdraustųjų žmonių skaičių iš praeitoje užduotyje atrinktų penkių įmonių, matoma, kad daugiausia apdraudusių žmonių yra "UAB CGTRADER". Šis skaičius bent dvigubai viršija kitų keturių įmonių apdraustųjų kiekį. "VOLTAS IT" ir "HARMONY LIFE" turi apyligį apdraustų žmonių skaičių. Paskutinėje vietoje yra įmonė "SELECT RINKA", kurios apdraustųjų žmonių skaičius yra maždaug septynis kartus mažesnis už pirmąją vietą užimančią įmonę.
 
 
 ### 4. Užduotis
